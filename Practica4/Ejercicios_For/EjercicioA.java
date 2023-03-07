87dba12ead47df80fa74f48d28f2732837837e14
Jos210 <126116044+Jos210@users.noreply.github.com>
2023-03-07 10:26:21 -0600

    Create EjercicioA.java

---
+++ Practica4/Ejercicios_For/EjercicioA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_For;
+
+/**
+ *
+ * @author Josue
+ */
+
+public class EjercicioA {
+    public static void main(String[] args) {
+        
+        //6,8,10,12,14,16,18,20,22,24,26,28,30
+        
+        for(int n=6;n<=30;n=n+2){
+            System.out.println("El numero par es "+n);
+        }
+    }
+    
+}
