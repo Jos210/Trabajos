ee89b51f12165aad049fb656430401293679f445
Josué <126116044+Jos210@users.noreply.github.com>
2023-06-14 08:04:39 -0600

    Add files via upload

---
+++ PracticaCadenas/Ejercicio3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cadenas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Josue
+ */
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingrese un texto ");
+        String cad1=sc.next();
+        
+        
+    }
+    
+}
