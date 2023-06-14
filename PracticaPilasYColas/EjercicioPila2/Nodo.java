7d11e8011744f559646fce84117811b20390466d
Josué <126116044+Jos210@users.noreply.github.com>
2023-06-14 08:21:33 -0600

    Create Nodo.java

---
+++ PracticaPilasYColas/EjercicioPila2/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioPila2;
+
+
+/**
+ *
+ * @author Josue
+ */
+public class Nodo {
+    int valor;
+    Nodo siguiente;
+}
