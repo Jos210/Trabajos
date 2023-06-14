ba496e88001bc29a4a26ec597ec665fbef074cba
Josué <126116044+Jos210@users.noreply.github.com>
2023-06-14 07:47:51 -0600

    Add files via upload

---
+++ PracticaFlujos/Ejercicio2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flujos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Josue
+ */
+
+public class Ejercicio2 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        int caracter;
+        FileInputStream lectura;
+       
+        lectura=new FileInputStream("C:\\Users\\CTPG\\Documents\\Ejercicios\\Binario1.txt");
+        
+        caracter=lectura.read();
+        
+        while(caracter!=-1){
+            System.out.print((char)caracter);
+            caracter=lectura.read();
+        }
+        System.out.println(caracter);
+        
+        lectura.close();
+    }
+    
+}
---
+++ PracticaFlujos/flujoByteBuffer.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flujos;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Josue
+ */
+public class flujoByteBuffer {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        int caracter;
+        BufferedInputStream lectura=null;
+        BufferedOutputStream escritura=null;
+        
+        //creando objetos de cada flujo de E/S
+        lectura=new BufferedInputStream(new FileInputStream("ejemplo.txt"));
+        escritura= new BufferedOutputStream( new FileOutputStream("ejemplo2.txt"));
+        
+        caracter=lectura.read();
+        
+        //hacer el recorrido de ficheros byte por byte hasta llegar al final 
+        
+        while(caracter!=-1){
+            escritura.write(caracter);
+            caracter=lectura.read();
+        }
+        System.out.println("El archivo fue copiado exitosamente");
+        lectura.close();
+        escritura.close();
+        
+    }
+    
+}
