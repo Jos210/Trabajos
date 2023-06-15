0552c993e600ae1ac3824bc6ab2cf77993d96a41
Josué <126116044+Jos210@users.noreply.github.com>
2023-06-14 23:09:40 -0600

    Create Nodo.java

---
+++ Proyecto/Actividad3/Nodo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividad3;
+
+/**
+ *
+ * @author Josue
+ */
+public class Nodo {
+
+    public Nodo(int dato1) {
+    }
+    int valor;
+    Nodo siguiente;
+    
+}
