5cf190b228354be6178bc4df37e7135da20b12da
Josué <126116044+Jos210@users.noreply.github.com>
2023-11-24 08:51:55 -0600

    Create principal.java

---
+++ Documentacion/principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 
+ */
+public class principal {
+    
+    principal fP = new principal();
+
+    public static procedimientos proce = new procedimientos();
+    
+    public static void main(String[] args) {
+
+       proce.menu();
+        
+    }
+    
+}
